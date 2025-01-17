35738f7157a45b883d802a79ea3804e759535a2e
Nikita <github@nikiv.dev>
2025-01-17 23:04:37 +0100

    db setup

--- app/auth/[auth]/auth-view.tsx
+++ app/auth/[auth]/auth-view.tsx
@@ -2,35 +2,34 @@
 
 import Link from "next/link"
 import { usePathname, useRouter } from "next/navigation"
-import { useCallback } from "react"
 
 import { authClient } from "@/lib/auth-client"
-import { AuthCard, AuthToastOptions } from "@daveyplate/better-auth-ui"
+import { AuthCard } from "@daveyplate/better-auth-ui"
 
-import { ToastAction } from "@/components/ui/toast"
-import { useToast } from "@/hooks/use-toast"
+// import { ToastAction } from "@/components/ui/toast"
+// import { useToast } from "@/hooks/use-toast"
 
 export default function AuthView() {
   const router = useRouter()
   const pathname = usePathname()
-  const { toast } = useToast()
+  // const { toast } = useToast()
 
   const callbackURL = "/"
 
-  const authToast = useCallback(
-    ({ variant, description, action }: AuthToastOptions) => {
-      toast({
-        variant,
-        description,
-        action: action && (
-          <ToastAction altText={action.label} onClick={action.onClick}>
-            {action.label}
-          </ToastAction>
-        ),
-      })
-    },
-    [toast]
-  )
+  // const authToast = useCallback(
+  //   ({ variant, description, action }: AuthToastOptions) => {
+  //     toast({
+  //       variant,
+  //       description,
+  //       // action: action && (
+  //       //   <ToastAction altText={action.label} onClick={action.onClick}>
+  //       //     {action.label}
+  //       //   </ToastAction>
+  //       // ),
+  //     })
+  //   },
+  //   [toast]
+  // )
 
   return (
     <main className="flex flex-col items-center my-auto p-4">
@@ -39,7 +38,7 @@ export default function AuthView() {
         pathname={pathname}
         navigate={router.push}
         providers={["github"]}
-        toast={authToast}
+        // toast={authToast}
         callbackURL={callbackURL}
         LinkComponent={Link}
         disableAnimation={true}
